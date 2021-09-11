df912a43f09716504345a44711cf7645fd5dfbb0
KARANAM AJAY NAG <ajaynagkaranam@gmail.com>
2021-09-11 20:15:36 +0530

    first commit

---
+++ Coding Ninjas/Pattern.cpp
@@ -0,0 +1,16 @@
+## Read input as specified in the question
+## Print the required output in given format
+int n;
+cin >> n;
+
+int i = 0;
+
+while(i<=n){
+    j = i;
+    while(j>=1){
+        cout<<'* ';
+        j--;
+    }
+    i++;
+    cout<<;
+}
